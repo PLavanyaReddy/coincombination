e2a6527b3b0fc0bb5fd981da860ccf6417433d08
unknown <lavanyareddy19994@gmail.com>
2020-07-14 13:43:53 +0530

    modified singlet combination

--- flip_coin.sh
+++ flip_coin.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-flip=$(( RANDOM%2 ))
-if [ $flip -eq 0 ]
-then
-	echo "Tails"
-else
-	echo "Heads"
-fi
+declare -A dict
+dict=(["H"]=0 ["T"]=0)
+read -p "Number of times to flip the coin" num
+for (( i=1;i<=$num;i++ ))
+do
+        flip=$(( RANDOM%2 ))
+        if [ $flip -eq 0 ]
+        then
+                key="T"
+        else
+                key="H"
+        fi
+        dict[$key]=$(( ${dict[$key]}+1 ))
+done
+for index in  ${!dict[@]}
+do
+        echo "Number of times $index appears is "${dict[$index]}
+        percent=`echo ${dict[$index]} $num | awk '{print $1/$2}'`
+        percentile=`echo $percent 100 | awk '{print $1*$2}'`
+        echo "The percentage of $index is "$percentile
+done
+
